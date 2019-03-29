9f3375a20963a549504c32596dc06c409e013322
gsarti <gabriele.sarti996@gmail.com>
2019-03-29 15:58:31 +0100

    Fixed script

--- Assignments/ex01/ex1.sh
+++ Assignments/ex01/ex1.sh
@@ -7,9 +7,9 @@ make
 for thread in 1 2 4 8 16 20
 do
     export OMP_NUM_THREADS=${threads}
-    echo ”−−−−−−−−−−−−−−−−−−”
-    echo ”Run with ${thread} thread(s)”
-    echo ”−−−−−−−−−−−−−−−−−−”
+    echo "−−−−−−−−−−−−−−−−−−"
+    echo "Run with ${thread} thread(s)"
+    echo "−−−−−−−−−−−−−−−−−−"
     ./ex1.x 1000000000
 done
 
