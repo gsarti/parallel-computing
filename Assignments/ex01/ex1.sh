43848541dacb986a3805f351571a33a52553e311
Gabriele Sarti <gsarti@login2.(none)>
2019-04-03 20:22:24 +0200

    Fixed ex1, now working

--- Assignments/ex01/ex1.sh
+++ Assignments/ex01/ex1.sh
@@ -6,7 +6,7 @@ make
 
 for thread in 1 2 4 8 16 20
 do
-    export OMP_NUM_THREADS=${threads}
+    export OMP_NUM_THREADS=${thread}
     echo "−−−−−−−−−−−−−−−−−−"
     echo "Run with ${thread} thread(s)"
     echo "−−−−−−−−−−−−−−−−−−"
