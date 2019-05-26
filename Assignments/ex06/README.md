95411519406f4facfabd6daac47fabc14052e65e
gsarti <gabriele.sarti996@gmail.com>
2019-05-26 23:44:28 +0200

    Finished exercise 6

--- Assignments/ex06/README.md
+++ Assignments/ex06/README.md
@@ -6,7 +6,21 @@ The purpose of the exercise was to implement a matrix transpose in CUDA that cou
 
 ## Results
 
-By exploiting the blocking for cache mechanism inside the GPU, we are able to double the bandwidth for the original matrix transposition performed with a naive implementation in CUDA, halving the time required to carry out the computation.
+The original naive transpose algorithm using a thread block of the size of the tile (32x32) achieved a bandwidth of roughly 50.11 GB/s.
+
+Table 1 presents the bandwidth results of execution for the improved version of the algorithm which is tuned to exploit blocking for cache in order to maximize the use of shared memory on the GPU.
+
+|  Block size | 2     | 4     | 8     | 16    | 32    |
+|-------------|-------|-------|-------|-------|-------|
+| 2           | 4.33  | 8.40  | 15.64 | 27.40 | 50.37 |
+| 4           | 7.62  | 15.01 | 28.17 | 53.00 | 81.05 |
+| 8           | 12.42 | 25.09 | 47.69 | 77.33 | 94.58 |
+| 16          | 18.36 | 35.60 | 60.90 | 77.28 | 78.43 |
+| 32          | 21.70 | 41.08 | 41.94 | 43.99 | 42.33 |
+
+As we can see, using a block of size 8x32 we achieve a bandwidth that is almost the double if compared to the naive implementation. This exercise shows how an intelligent use of the cache may greatly improve the results for this kind of operation on a GPU.
+
+Even better performance may be achieved by implementing a **coalesced version** of the transpose algorithm, that greatly reduces the number of global memory accesses by coalescing half-warps of threads into a couple transactions. This even better version is described and implemented thoroughly in an [official NVIDIA document](https://www.cs.colostate.edu/~cs675/MatrixTranspose.pdf) and reaches easily 130 GB/s of bandwidth. The code wasn't test since it was out of the scope for the current exercise and didn't require a lot of thinking, being already implemented in the said document.
 
 ## Reproducibility
 
--- Assignments/ex06/ex6.cu
+++ Assignments/ex06/ex6.cu
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
-#define TILE 32
-#define BLOCK_X 8
-#define BLOCK_Y 16
-#define N 8192
+#define TILE 32 // Thread block dimension
+#define N 8192 // Side of the matrix
+#define MATSIZE N * N // Total size of the matrix
+#define MEMSIZE MATSIZE * sizeof(double) // Size of matrix in memory
 
+// Generic function to be called for bandwidth testing on GPUs.
+typedef void (*kernelFunc)(double *, double *, int);
+
+/**
+ * @brief Performs an optimized version of a matrix transposition.
+ * @param a The input matrix.
+ * @param b The transposed matrix in output.
+ * @param size The size of the matrix side.
+ *
+ * By exploiting GPU shared memory we may decompose the transposition in
+ * multiple submatrices transpositions, minimizing global memory accesses
+ * by doing them simultaneously for same-tile threads.
+ */
 __global__ void transposeOptimized(double *a, double *b, int size) 
 {
-    __shared__ double tile[BLOCK_X][BLOCK_Y];
+    __shared__ double tile[TILE][TILE];
     int col = blockIdx.x * blockDim.x + threadIdx.x;
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     tile[threadIdx.x][threadIdx.y] = a[row * size + col];
@@ -15,6 +28,12 @@ __global__ void transposeOptimized(double *a, double *b, int size)
     b[col * size + row] = tile[threadIdx.x][threadIdx.y];
 }
 
+/**
+ * @brief Performs a naive version of a matrix transposition on GPU.
+ * @param a The input matrix.
+ * @param b The transposed matrix in output.
+ * @param size The size of the matrix side.
+ */
 __global__ void transposeNaive(double *a, double *b, int size) 
 {
     int row = blockIdx.x * blockDim.x + threadIdx.x;
@@ -22,6 +41,12 @@ __global__ void transposeNaive(double *a, double *b, int size)
     b[col * size + row] = a[row * size + col];
 }
 
+/**
+ * @brief Performs a serial version of a matrix transposition on CPU.
+ * @param a The input matrix.
+ * @param b The transposed matrix in output.
+ * @param size The size of the matrix side.
+ */
 void transposeCpu(double *a, double *b, int size) 
 {
     for (int i = 0; i < size; ++i)
@@ -29,6 +54,12 @@ void transposeCpu(double *a, double *b, int size)
         b[j * size + i] = a[i * size + j];
 }
 
+/**
+ * @brief Validates the equality of two matrices in input.
+ * @param a Matrix a.
+ * @param b Matrix b.
+ * @param size The size of the matrix side.
+ */
 int isCorrect(double * a, double * b, int size)
 {
     for(int i = 0; i < size; ++i)
@@ -38,73 +69,84 @@ int isCorrect(double * a, double * b, int size)
     return 1;
 }
 
-int main(int argc, char *argv[]) {
-
-    double * h_in, * h_out_naive, * h_out_opt;
-    double * dev_in, * dev_out_naive, * dev_out_opt;
+/**
+ * @brief Tests execution time and bandwidth of a transposition kernel on a GPU. 
+ * @param kernel The kernel to be tested.
+ * @param kernelName The name of the kernel to be tested.
+ * @param block_x The x-dimension of the block used to perform blocking for cache.
+ * @param block_y The y-dimension of the block used to perform blocking for cache.
+ *
+ * The transposition is performed as specified by the kernel function and then is
+ * validated against a correctly-transposed matrix. GPU time and bandwidth are
+ * provided as outputs of the function.
+ */
+int testCudaBandwidth(kernelFunc kernel, const char * kernelName, int block_x, int block_y)
+{
+    double * h_in, * h_out;
+    double * dev_in, * dev_out;
     double * cpu;
-    int size = N * N;
-    int memsize = size * sizeof(double); 
-    
-    dim3 block(TILE, TILE);
+
+    dim3 block(block_x, block_y);
     dim3 grid(N / block.x, N / block.y);
-    dim3 blockOpt(BLOCK_X, BLOCK_Y);
-    dim3 gridOpt(N / blockOpt.x, N / blockOpt.y);
+
+    h_in = (double *)malloc(MEMSIZE);
+    h_out = (double *)malloc(MEMSIZE);
+    cpu = (double *)malloc(MEMSIZE);
+    cudaMalloc((void **)&dev_in, MEMSIZE);
+    cudaMalloc((void **)&dev_out, MEMSIZE);
     
-    h_in = (double *)malloc(memsize);
-    h_out_naive = (double *)malloc(memsize);
-    h_out_opt = (double *)malloc(memsize);
-    cpu = (double *)malloc(memsize);
-    cudaMalloc((void **)&dev_in, memsize);
-    cudaMalloc((void **)&dev_out_naive, memsize);
-    cudaMalloc((void **)&dev_out_opt, memsize);
-    
-    for(int i = 0; i < size; ++i)
+    // Fill input matrix with some indices (for validating transposition).
+    for(int i = 0; i < MATSIZE; ++i)
         h_in[i] = i;
     
-    cudaMemcpy(dev_in, h_in, memsize, cudaMemcpyHostToDevice);
-    
+    // Initial setup of input matrix and timing events.
+    cudaMemcpy(dev_in, h_in, MEMSIZE, cudaMemcpyHostToDevice);    
     cudaEvent_t start, stop;
     float exec_time = 0;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
 
-    printf("Transposing matrix on CPU for validation...\n");
+    // Print some informations about the current task.
+    printf("\nTransposing matrix on CPU for validation...\n");
     transposeCpu(h_in, cpu, N);
-    printf("\nMatrix size: %dx%d, tile: %dx%d\n", N, N, TILE, TILE);
-    printf("\nKernel: Naive transpose\n\n");
+    printf("\nMatrix size: %dx%d, tile: %dx%d, block:%dx%d \n", N, N, TILE, TILE, block_x, block_y);
+    printf("\nKernel: %s\n\n", kernelName);
 
+    // Time kernel execution.
     cudaEventRecord(start);
-    transposeNaive<<<grid, block>>>(dev_in, dev_out_naive, N);
+    kernel<<<grid, block>>>(dev_in, dev_out, N);
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
     cudaEventElapsedTime(&exec_time, start, stop);
-    cudaMemcpy(h_out_naive, dev_out_naive, memsize, cudaMemcpyDeviceToHost);
-    printf("Naive transpose: %s\n", isCorrect(h_out_naive, cpu, N) ? "CORRECT" : "FAIL");
+    cudaMemcpy(h_out, dev_out, MEMSIZE, cudaMemcpyDeviceToHost);
+    printf("%s: %s\n", kernelName, isCorrect(h_out, cpu, N) ? "CORRECT" : "FAIL");
     printf("GPU Time: %f\n", exec_time);
-    printf("Bandwidth (GB/s): %f\n", memsize * 2 / exec_time / 1000000);
 
-    free(h_out_naive);
-    cudaFree(dev_out_naive);
+    // Bandwidth given by reading and writing a matrix during exec_time,
+    // converted to GB/s for readability.
+    printf("Bandwidth (GB/s): %f\n", MEMSIZE * 2 / exec_time / 1000000);
+    printf("-------------------------------\n");
 
-    printf("\nKernel: Optimized transpose\n\n");
-
-    cudaEventRecord(start);
-    transposeOptimized<<<gridOpt, blockOpt>>>(dev_in, dev_out_opt, N);
-    cudaEventRecord(stop);
-    cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&exec_time, start, stop);
-    cudaMemcpy(h_out_opt, dev_out_opt, memsize, cudaMemcpyDeviceToHost);
-    printf("Optimized transpose: %s\n", isCorrect(h_out_opt, cpu, N) ? "CORRECT" : "FAIL");
-    printf("GPU Time: %f\n", exec_time);
-    printf("Bandwidth (GB/s): %f\n", memsize * 2 / exec_time / 1000000);
-
+    // Cleanup
     free(h_in);
-    free(h_out_opt);
+    free(h_out);
     free(cpu);
     cudaFree(dev_in);
-    cudaFree(dev_out_opt);
+    cudaFree(dev_out);
     cudaEventDestroy(start);
     cudaEventDestroy(stop);
+    return 0;
+}
+
+int main(int argc, char *argv[]) 
+{
+    testCudaBandwidth(&transposeNaive, "Naive Transpose", 32, 32);
+    
+    int a[5] = {2, 4, 8, 16, 32};
+
+    for(int i = 0; i < 5; ++i)
+        for(int j = 0; j < 5; ++j)
+            testCudaBandwidth(&transposeOptimized, "Optimized Transpose", a[i], a[j]);
+
     return 0;
 }
